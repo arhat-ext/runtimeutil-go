e07be88640c43a20ba92a02f82d46ad11fbf389f
Jeffrey Stoke <me@arhat.dev>
2020-12-05 20:02:45 +0100

    cicd: Add build test for all platforms

--- scripts/test.mk
+++ scripts/test.mk
@@ -23,3 +23,11 @@ install.fuzz:
 
 test.fuzz:
 	sh scripts/fuzz.sh run
+
+test.build:
+	go tool dist list | xargs -Ipair \
+		sh -c '\
+        	CGO_ENABLED=false \
+            GOOS=$(echo pair | cut -d/ -f1) \
+            GOARCH=$(echo pair | cut -d/ -f2) \
+            echo "Building pair" && go build ./...'
